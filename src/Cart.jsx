aa0a3d62eef8791c05ac42bdfda487f8de8460c7
Vlad993 <15590551-Vlad993@users.noreply.replit.com>
2023-10-20 06:22:48 +0000

    starting  cart component

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 
 const Cart = () => {
+  const [isModalDisplayed, setIsModalDisplayed] = useState(false);
+
+useEffect(() => {
+  console.log(isModalDisplayed);
+  
+}, [isModalDisplayed])
+
+  
+  const handleModalDisplay = (item) => {
+  setIsModalDisplayed(true);
+  }
+  
   return (
-    <div>
-      <button className="cart-button">
-        C
-      </button>
-    </div>
-  );
-};
+  <div className="cart">
+    <button className="cart-button" onClick={handleModalDisplay}>
+      C
+    </button>
+
+    {isModalDisplayed && <div className="cart-modal">
+    <h1>Test</h1>
+    
+    
+</div>}
+  
+  
+  
+  
+  
+  </div>
+  
+  
+  )
+}
 
 export default Cart;
